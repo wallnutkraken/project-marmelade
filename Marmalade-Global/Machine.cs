ef1a8891780bc012fc77d70dd0cc2d3ca2502262
Mikulas Tomanka <Mikulas@gmail.com (home)>
2016-02-02 13:44:30 +0100

    Created employeeschedule classes and employeeschedule controller

---
+++ Marmalade-Global/Machine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marmalade_Global
+{
+    class Machine
+    {
+        public string PlaceHolder { get; set; }
+
+    }
+}
