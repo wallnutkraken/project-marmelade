1ba92b91f4621906beb50401deac3fcbcd5eb1ee
Pavel <you@example.com>
2016-02-08 14:08:28 +0100

    ProductionLine and ProductionCycle classes created

---
+++ Marmalade-Global/ProductionCycle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marmalade_Global
+{
+    class ProductionCycle
+    {
+        // public List<ProductionTask> AllWeeklyTasks { get; set; } = new List<ProductionTask>();
+        public int AmountOfWProducts { get; set; }
+       // public Product ProductsProduced { get; set; }
+    }
+}
---
+++ Marmalade-Global/ProductionLine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marmalade_Global
+{
+    class ProductionLine
+    {
+        //   Product Product;
+        public int Amount { get; set; }
+        public DateTime Week { get; set; }
+    }
+}
