e3ae673af1a3f51c7874c72cc99c9685614e8740
Emile <emil44d8@edu.eal.dk>
2016-02-11 10:03:12 +0100

    Added unfinished CreateMachine UI

---
+++ Marmalade-Global/Machine/CreateMachine.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Marmalade_Global.Machine
+{
+    /// <summary>
+    /// Interaction logic for CreateMachine.xaml
+    /// </summary>
+    public partial class CreateMachine : Window
+    {
+        public CreateMachine()
+        {
+            InitializeComponent();
+            typeComboBox.ItemsSource = UtilityController.EnumToList<MachineType>();
+            typeComboBox.SelectedIndex = 0;
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Marmalade-Global/Machine/MachineController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marmalade_Global.Machine
+{
+    static class MachineController
+    {
+        /// <summary>
+        /// Creates a machine from parameters and adds it to the database.
+        /// </summary>
+        /// <returns>Bool reporting whether the operation was successful or not</returns>
+        public static bool AddMachine(MachineType type)
+        {
+            bool? result = null;
+            MachineEntry entry = new MachineEntry(type);
+            try
+            {
+                Database.AddMachine(entry);
+                result = true;
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+
+            return (bool)result;
+        }
+    }
+}
